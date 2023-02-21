623c5f555608bb6bd2beef103574571fa8ea16f3
SUSHMITHA <76254481+sushmitha286@users.noreply.github.com>
2023-02-21 16:36:43 +0530

    Add files via upload

---
+++ jsoutput/program4.js
@@ -0,0 +1,10 @@
+document.getElementById("demo").innerHTML="hello javascript";
+//get a output is 11
+document.write(5+6);  
+//by get all ae delete get 18.
+function myfunc(){
+    document.write(9+9);
+}
+function myfunction(){
+window.alert("not present any value");
+}
