6861e54e726df5985827e1ff8445843b48c0c2dd
SUSHMITHA <76254481+sushmitha286@users.noreply.github.com>
2023-02-21 11:12:07 +0530

    Delete program2.js

--- program2.js
+++
@@ -1,6 +0,0 @@
-function bulb_on(){
-    document.getElementById("bulb").src = 'https://i.postimg.cc/6QyTynzr/bulb-on.png';
-}
-function bulb_off(){
-    document.getElementById('bulb').src='https://i.postimg.cc/KjK1wL3c/bulb-off.png';
-}
