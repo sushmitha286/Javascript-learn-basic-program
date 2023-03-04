774ee8bc7780f3b719ae837d73abdc53a898e715
SUSHMITHA <76254481+sushmitha286@users.noreply.github.com>
2023-03-04 22:42:05 +0530

    Add files via upload

---
+++ jsdatesetmethods/program18.js
@@ -0,0 +1,20 @@
+const A = new Date();
+A.setFullYear(2020);
+document.getElementById('a').innerHTML = A
+const B = new Date();
+document.getElementById('b').innerHTML = B;
+const C = new Date();
+C.setMonth(11);
+document.getElementById('c').innerHTML = C;
+const D = new Date();
+D.setDate(15)
+document.getElementById('d').innerHTML = D;
+const E = new Date();
+E.setHours(21)
+document.getElementById('e').innerHTML = E;
+const F = new Date();
+F.setMinutes(54)
+document.getElementById('f').innerHTML = F;
+const G = new Date();
+G.setSeconds(34)
+document.getElementById('g').innerHTML = G;
