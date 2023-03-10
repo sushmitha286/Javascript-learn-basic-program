710b5fbd384c9e7dd660bbafe82158afe113468d
SUSHMITHA <76254481+sushmitha286@users.noreply.github.com>
2023-03-10 21:21:58 +0530

    Add files via upload

---
+++ Socialmediashareiconanimation/program23.js
@@ -0,0 +1,32 @@
+const youtubeIcon = document.getElementById('youtube-icon');
+youtubeIcon.addEventListener('click', function(event) {
+  event.preventDefault();
+  window.open('https://www.youtube.com', '_blank');
+});
+const googleIcon = document.getElementById('google-icon');
+googleIcon.addEventListener('click', function(event) {
+  event.preventDefault();
+  window.open('https://www.google.com', '_blank');
+});
+const whatsappIcon = document.getElementById('whatsapp-icon');
+whatsappIcon.addEventListener('click', function(event) {
+  event.preventDefault();
+  window.open('https://www.whatsapp.com', '_blank');
+});
+const instagramIcon = document.getElementById('instagram-icon');
+instagramIcon.addEventListener('click', function(event) {
+  event.preventDefault();
+  window.open('https://www.instagram.com', '_blank');
+});
+const twitterIcon = document.getElementById('twitter-icon');
+twitterIcon.addEventListener('click', function(event) {
+  event.preventDefault();
+  window.open('https://twitter.com', '_blank');
+});
+const githubIcon = document.getElementById('github-icon');
+githubIcon.addEventListener('click', function(event) {
+  event.preventDefault();
+  window.open('https://github.com', '_blank');
+});
+  // Get all social media links
+ 
